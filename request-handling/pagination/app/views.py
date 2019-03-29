f4fc7dee4a2439a62648cc42a71099bf7f22f4ba
Alexey <alex.erm@yandex.ru>
2019-03-29 20:50:48 +0300

    fix 3rd task in ./requset-handling

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -3,23 +3,25 @@ import csv
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
 
-from app.settings import BUS_STATION_CSV
+from django.conf import settings
+from django.core.paginator import Paginator
 
-def create_database():
+
+def create_paginator():
 	database = []
-	with open(BUS_STATION_CSV, newline='') as csvfile:
+	with open(settings.BUS_STATION_CSV, newline='') as csvfile:
 		reader = csv.DictReader(csvfile)
 		for row in reader:
 			database.append({'Name': row['Name'], 'Street': row['Street'], 'District': row['District']})
-	return database
+	return Paginator(database, 10)
 
-database = create_database()
 
 
 def index(request):
     return redirect(reverse(bus_stations))
 
 def bus_stations(request):
+	paginator = create_paginator()
 	current_page = int(request.GET.get('page'))
 	next_page_url = f'bus_stations?page={current_page + 1}'
 
@@ -29,7 +31,7 @@ def bus_stations(request):
 		prev_page_url = f'bus_stations?page={current_page - 1}'
 
 	return render_to_response('index.html', context = {
-		'bus_stations': database[0 + current_page: 10 + current_page],
+		'bus_stations': paginator.page(current_page),
 		'current_page': current_page,
 		'prev_page_url': prev_page_url,
 		'next_page_url': next_page_url
