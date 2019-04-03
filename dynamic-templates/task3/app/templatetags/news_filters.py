12f4403de09b721d632e818bfaa8bf1a21d29c00
Alexey <alex.erm@yandex.ru>
2019-04-03 17:56:33 +0300

    fix 3rd task in ./dyanimc-templates

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,17 +1,15 @@
 from django import template
 from datetime import timedelta
-import datetime
+from datetime import datetime
 import time
 
 register = template.Library()
 
-def create_datetime(tuple):
-	return datetime.datetime(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5])
-
 @register.filter
 def format_date(value):
-	post_datetime = create_datetime(time.gmtime(value))
-	delta = datetime.datetime.today() - post_datetime
+	post_datetime = datetime.fromtimestamp(value)
+
+	delta = datetime.today() - post_datetime
 	delta_minutes = delta.seconds % 3600
 	delta_hours = delta.seconds // 3600
 
@@ -33,14 +31,14 @@ def score_filter(score, default):
 			return 'нейтрально'
 		else:
 			return 'хорошо'
-	else:
-		return default
+			
+	return default
 
 
 @register.filter
-def format_num_comments(value):
+def format_num_comments(value, default):
     if value == 0:
-    	return 'Оставьте комментарий'
+    	return default
     elif value > 0 and value < 50:
     	return value
     elif value >=50:
@@ -49,6 +47,9 @@ def format_num_comments(value):
 
 @register.filter
 def format_selftext(text, count):
+	if count > len(text):
+		return text
+
 	result = text.split(' ')
 	first = ' '.join(result[0 : count])
 	last = ' '.join(result[len(result) - count : len(result)])
