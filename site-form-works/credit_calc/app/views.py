08d81f54ab814eb5a9151859571aa7eae1dcd26f
Alexey <alex.erm@yandex.ru>
2019-04-03 17:42:19 +0300

    fix 1 task in ./site-for-works

--- site-form-works/credit_calc/app/views.py
+++ site-form-works/credit_calc/app/views.py
@@ -8,21 +8,18 @@ class CalcView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         form = CalcForm(request.GET)
+
+        summa = int(request.GET.get('initial_fee', 0))
+        percent = int(request.GET.get('rate', 0)) / 100
+        months = int(request.GET.get('months_count', 1))
+
+        common_result = summa + summa * percent
+        result = common_result / months
+
         context = {
             'form': form,
+            'result': result,
+            'common_result': common_result
         }
-        print('kwargs', **kwargs)
-        if self.kwargs:
-            sum = int(request.GET.get('initial_fee'))
-            percent = int(request.GET.get('rate')) / 100
-            months = int(request.GET.get('months_count'))
-
-            result = (sum + sum * percent) / months
-
-            context = {
-                'form': form,
-                'result': result,
-                'common_result': sum + sum * percent
-            }
 
         return self.render_to_response(context)
